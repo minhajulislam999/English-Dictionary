a9c4be4debb33a73554576b6392636d84ab67654
minhajul islam <mm01882390860@gmail.com>
2026-02-27 19:42:39 +0600

    error Handle

--- script/script.js
+++ script/script.js
@@ -41,11 +41,11 @@ const displayLevleWord = (words) => {
         const card = document.createElement("div");
         card.innerHTML = `
          <div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4">
-      <h2 class="font-bold text-xl">${word.word}</h2>
+      <h2 class="font-bold text-xl">${word.word ? word.word : "kono shobdo nai"}</h2>
       <p class="font-semibold">Meaning /Pronounciation</p>
 
       <div class="text-2xl font-medium bangla-font">
-        "${word.meaning}/ ${word.pronunciation}"
+        "${word.meaning ? word.meaning : "kono meaning nai"}/ ${word.pronunciation ? word.pronunciation : "kono pronunciation nai"}"
       </div>
       <div class="flex justify-between items-center">
         <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
